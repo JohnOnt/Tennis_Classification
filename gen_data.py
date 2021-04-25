a72b3f161b095964545bed9efda66b9d70d9ad9f
JohnOnt <jhontiveros28@gmail.com>
2021-04-25 00:31:01 -0400

    logistic regression with classes implemented

--- gen_data.py
+++ gen_data.py
@@ -4,9 +4,10 @@ from tqdm import tqdm
 
 # rankings = pd.read_csv('ATP_Rankings2017.csv')
 rankings = pd.read_csv('Elo_Rankings2017.csv')
+pclasses = pd.read_csv('player_classifications.csv')
 
 def gen_outcomes():
-    players = rankings.name.values
+    players = rankings.name.values[:101]
 
     colnames = ['Player1', 'Player2', 'Outcome']
     Outcomes = pd.DataFrame(index=[''], columns=colnames)
@@ -50,12 +51,12 @@ def gen_outcomes():
 
 
 def gen_elo_win_data():
-    wins_matrix = np.array([[0, 1]])
+    wins_matrix = np.array([[0, 1, 2, 3]])
 
     for year in tqdm(np.arange(2004, 2018)):
         matches = pd.read_csv('tennis_atp-master/atp_matches_' + str(year) + '.csv')
 
-        wins_sub_matrix = np.zeros((np.shape(matches)[0], 2))
+        wins_sub_matrix = np.zeros((np.shape(matches)[0], 4))
 
         for i, match in matches.iterrows():
             pw = match.winner_name
@@ -63,24 +64,30 @@ def gen_elo_win_data():
 
             if ((pw in (rankings.name.values)) == False) | ((pl in (rankings.name.values)) == False):
                 # We don't know one of the elo ratings of the players so we pass it off
-                wins_sub_matrix[i] = [np.nan, np.nan]
+                wins_sub_matrix[i] = [np.nan, np.nan, np.nan, np.nan]
+                continue
+
+            elif ((pw in (pclasses.Player.values)) == False) | ((pl in (pclasses.Player.values)) == False):
+                # We don't know one of the class of the players so we pass it off
+                wins_sub_matrix[i] = [np.nan, np.nan, np.nan, np.nan]
                 continue
 
             pw_elo = rankings.points[rankings.name == pw].values[0]
             pl_elo = rankings.points[rankings.name == pl].values[0]
 
+            pw_class = pclasses.Class[pclasses.Player == pw].values[0]
+            pl_class = pclasses.Class[pclasses.Player == pl].values[0]
             # You can add court surface values but that would be a lot more work
 
-            wins_sub_matrix[i] = [(pw_elo - pl_elo), 1]
+            wins_sub_matrix[i] = [(pw_elo - pl_elo), pw_class, pl_class, 1]
         
         wins_matrix = np.append(wins_matrix, wins_sub_matrix, axis=0)
 
 
-    colnames = ['elo_diff', 'winner']
+    colnames = ['elo_diff', 'winner_class', 'loser_class', 'winner']
     elo_wins_df = pd.DataFrame(wins_matrix[1:], columns=colnames)
     elo_wins_df = elo_wins_df.dropna()
     elo_wins_df.to_csv('elo_wins.csv', index=False)
 
 
-def attach_classifications():
-    pass
+gen_elo_win_data()
